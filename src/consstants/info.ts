655522345e084f35cf64fe4861f0ac0ceb0586f6
Timur Bapanov <timson995@gmail.com>
2025-09-26 22:52:25 +0600

    support link and header

---
+++ src/consstants/info.ts
@@ -0,0 +1,9 @@
+
+export const termsUpdated = '2025-09-22'
+export const privacyUpdated = '2025-09-22'
+export const supportUpdated = '2025-09-22'
+export const emailContact = 'timur.bapanov@gmail.com'
+export const company = 'Bapanov Timur (operating as Deadfit)'
+export const copyright = 'Deadfit. All rights reserved.'
+export const appName = 'Deadfit'
+
