44665f9d74b46ba46b77f59d097dd56d4c91ed61
Timur Bapanov <timson995@gmail.com>
2025-10-06 20:28:33 +0600

    Added link to AppStore

--- src/consstants/info.ts
+++ src/consstants/info.ts
@@ -6,4 +6,5 @@ export const emailContact = 'timur.bapanov@gmail.com'
 export const company = 'Bapanov Timur (operating as Deadfit)'
 export const copyright = 'Deadfit. All rights reserved.'
 export const appName = 'Deadfit'
-
+export const AppStoreLink = 'https://apps.apple.com/us/app/deadfit-workouts-logs-goals/id6747721349'
+export const GooglePlayLink = null
